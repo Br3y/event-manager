160d4f3bb1a4288623040f6b602b8eb18e2f9524
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-03-12 14:07:52 +0800

    test: skipping the header row part 2

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -4,8 +4,12 @@ puts "Event Manager Initialized."
 # puts contents
 
 lines = File.readlines("./event_attendees.csv")
+
+row_index = 0
 lines.each do |line|
-  next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
+  row_index = row_index + 1
+  next if row_index == 1
+  # next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
   columns = line.split(",")
   first_name = columns[2]
   puts first_name
