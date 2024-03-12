689857648bc915a0af15a50c43c45281573e6228
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-03-12 14:20:04 +0800

    test: Parsing with CSV
    Switching over to use the CSV library

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,13 +1,9 @@
-puts "Event Manager Initialized."
+require 'csv'
 
-# contents = File.read("./event_attendees.csv")
-# puts contents
+puts "EventManager Initialized."
 
-lines = File.readlines("./event_attendees.csv")
-
-lines.each_with_index do |line, index|
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
+contents = CSV.open("./event_attendees.csv", headers: true)
+contents.each do |row|
+  name = row[2]
   puts name
 end
