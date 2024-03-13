b47a9c4643dba62d01e03711e9f10da29d61937a
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-03-13 08:43:23 +0800

    feat: cleaning zip codes
    handling missing zip codes that are less/greater than length of 5.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,5 +6,15 @@ contents = CSV.open('./event_attendees.csv', headers: true, header_converters: :
 contents.each do |row|
   name = row[:first_name]
   zip = row[:zipcode]
+
+  if zip.nil?
+    zip = "00000"
+  elsif zip.length > 5
+    zip[0..4]
+  elsif zip.length < 5
+    until zip.length == 5
+      zip += "0"
+    end
+  end
   puts "#{name} #{zip}"
 end
