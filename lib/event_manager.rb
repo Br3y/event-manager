c48fac9fc6749dc72d984f3a731d88d2f33ad795
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-03-13 08:58:55 +0800

    refactor: moving clean zip codes to a function

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,20 +1,23 @@
 require 'csv'
 
-puts "EventManager Initialized."
-
-contents = CSV.open('./event_attendees.csv', headers: true, header_converters: :symbol)
-contents.each do |row|
-  name = row[:first_name]
-  zip = row[:zipcode]
-
+def clean_zipcodes(zip)
   if zip.nil?
     zip = "00000"
   elsif zip.length > 5
     zip[0..4]
   elsif zip.length < 5
-    until zip.length == 5
-      zip += "0"
-    end
+    zip.rjust(5, "0")
+  else
+    zip
   end
+end
+
+puts "EventManager Initialized."
+
+contents = CSV.open('./event_attendees.csv', headers: true, header_converters: :symbol)
+contents.each do |row|
+  name = row[:first_name]
+  zip = clean_zipcodes(row[:zipcode])
+
   puts "#{name} #{zip}"
 end
