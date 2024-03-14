3a8c006474a33f1bb3273fa798a9f6f3e6e9d8f9
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-03-15 02:18:00 +0800

    refactor: moving displaying legislators to a function(def method)

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,19 +1,13 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
-
 def clean_zipcodes(zip)
   zip.to_s.rjust(5, "0")[0..4]
 end
 
-puts "EventManager Initialized."
-
-contents = CSV.open('./event_attendees.csv', headers: true, header_converters: :symbol)
-contents.each do |row|
-  name = row[:first_name]
-  zip = clean_zipcodes(row[:zipcode])
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
     legislators = civic_info.representative_info_by_address(
@@ -29,6 +23,15 @@ contents.each do |row|
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
-  
-  puts "#{name} #{zip} #{legislator_strings}"
+end
+
+puts "EventManager Initialized."
+
+contents = CSV.open('./event_attendees.csv', headers: true, header_converters: :symbol)
+contents.each do |row|
+  name = row[:first_name]
+  zip = clean_zipcodes(row[:zipcode])
+  legislators = legislators_by_zipcode(zip)
+
+  puts "#{name} #{zip} #{legislators}"
 end
