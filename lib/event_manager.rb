6b6b38081c50419abd68783d66110d1479f395bc
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-03-14 23:16:30 +0800

    feat: showing all legislators in a zip code

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,8 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 def clean_zipcodes(zip)
   zip.to_s.rjust(5, "0")[0..4]
@@ -11,5 +15,12 @@ contents.each do |row|
   name = row[:first_name]
   zip = clean_zipcodes(row[:zipcode])
 
-  puts "#{name} #{zip}"
+  legislators = civic_info.representative_info_by_address(
+    address: zip,
+    levels: 'country',
+    roles: ['legislatorUpperBody', 'legislatorLowerBody']
+  )
+  legislators = legislators.officials
+
+  puts "#{name} #{zip} #{legislators}"
 end
