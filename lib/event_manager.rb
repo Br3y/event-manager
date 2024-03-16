72f0c4157e5fba7209d65ab115007a7e39ff2316
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-03-17 01:14:06 +0800

    refactor: use rubocop and line space for better readability

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,7 +3,7 @@ require 'google/apis/civicinfo_v2'
 require 'erb'
 
 def clean_zipcodes(zip)
-  zip.to_s.rjust(5, "0")[0..4]
+  zip.to_s.rjust(5, '0')[0..4]
 end
 
 def legislators_by_zipcode(zip)
@@ -14,24 +14,28 @@ def legislators_by_zipcode(zip)
     legislators = civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+      roles: %w[legislatorUpperBody legislatorLowerBody]
     ).officials
-  rescue
+  rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
 
-puts "EventManager Initialized."
+puts 'EventManager Initialized.'
 
-template_letter = File.read("../form_letter.erb")
+template_letter = File.read('../form_letter.erb')
 erb_template = ERB.new(template_letter)
 
 contents = CSV.open('./event_attendees.csv', headers: true, header_converters: :symbol)
 contents.each do |row|
+
   name = row[:first_name]
+
   zip = clean_zipcodes(row[:zipcode])
+
   legislators = legislators_by_zipcode(zip)
 
   form_letter = erb_template.result(binding)
+  
   puts form_letter
 end
