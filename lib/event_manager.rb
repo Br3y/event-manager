9c641cdfa4418d79d747ec752a7991b5d2445cd3
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-05-06 14:27:20 +0800

    feat(rb): Displaying the zip codes of all atendees

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -61,3 +61,16 @@ puts 'Event Manager Initialized!'
 #   name = row[:first_name]
 #   puts name
 # end
+
+# # Displaying the zip codes of all attendees
+# contents = CSV.open(
+#   "event_attendees.csv",
+#   headers: true,
+#   header_converters: :symbol
+# )
+
+# contents.each do |row|
+#   name = row[:first_name]
+#   zip = row[:zipcode]
+#   puts "#{name} #{zip}"
+# end
