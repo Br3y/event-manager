cbcc468b32b86112e8e488c3d916705a5f77fde6
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-05-10 16:28:00 +0800

    feat: installing google api client and showing all legislators in a zip code
    add begin/rescue to handle error of nil zipcode

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
 
 puts 'Event Manager Initialized!'
 
@@ -97,6 +98,10 @@ puts 'Event Manager Initialized!'
 
 
 # Iteration 3: Using Google's Civic Information
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
+
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
 end
@@ -110,6 +115,17 @@ contents = CSV.open(
 contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
+  
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+  rescue 
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
 
-  puts "#{name} #{zipcode}"
+  puts "#{name} #{zipcode} #{legislators}"
 end
