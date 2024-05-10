fffca253d9b13886ec712e354b89aa5134a6732a
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-05-10 14:04:37 +0800

    feat: handling good and bad zip codes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -87,9 +87,11 @@ contents.each do |row|
   name = row[:first_name]
   zip = row[:zipcode]
 
-  # if the zip code is exactly five digits, assume that it is ok
-  # if the zip code is more than five digits, truncate it to the first five digits
-  # if the zip code is less than five digits, add zeros to the front until it becomes five digits
- 
+  if zip.length < 5
+    zip = zip.rjust(5, "0")
+  elsif zip.length > 5
+    zip = zip[0..4]
+  end
+
   puts "#{name} #{zip}"
 end
