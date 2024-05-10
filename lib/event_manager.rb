ee8ab92512aa807ee5685458987b3d8fe05d24ae
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-05-10 16:16:44 +0800

    feat: Iteration 3: Using Google's Civic Information

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -94,3 +94,22 @@ puts 'Event Manager Initialized!'
 
 #   puts "#{name} #{zip}"
 # end
+
+
+# Iteration 3: Using Google's Civic Information
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+contents = CSV.open(
+  "event_attendees.csv",
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+
+  puts "#{name} #{zipcode}"
+end
