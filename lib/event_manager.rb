ad9701190095af2b6fcbfb6a8307fffbc23b822f
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-05-10 14:03:17 +0800

    feat: cleaning up our zip codes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -74,3 +74,18 @@ puts 'Event Manager Initialized!'
 #   zip = row[:zipcode]
 #   puts "#{name} #{zip}"
 # end
+
+
+# Iteration 2: Cleaning up our zip codes
+contents = CSV.open(
+  "event_attendees.csv",
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zip = row[:zipcode]
+
+  puts "#{name} #{zip}"
+end
