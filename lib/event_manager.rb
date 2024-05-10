93e010f7ad545d136a76f3a3374d712031b9f78b
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-05-10 14:03:39 +0800

    feat: pseudocode for cleaning zip codes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -87,5 +87,9 @@ contents.each do |row|
   name = row[:first_name]
   zip = row[:zipcode]
 
+  # if the zip code is exactly five digits, assume that it is ok
+  # if the zip code is more than five digits, truncate it to the first five digits
+  # if the zip code is less than five digits, add zeros to the front until it becomes five digits
+ 
   puts "#{name} #{zip}"
 end
