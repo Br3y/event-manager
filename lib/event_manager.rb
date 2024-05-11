ceea6165f05e72faeee30a2c08b5024cf27abc3c
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-05-11 15:23:47 +0800

    comment out iteration 3

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -99,45 +99,45 @@ puts 'Event Manager Initialized!'
 
 # Iteration 3: Using Google's Civic Information
 
-def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5, "0")[0..4]
-end
+# def clean_zipcode(zipcode)
+#   zipcode.to_s.rjust(5, "0")[0..4]
+# end
 
-def legislator_by_zipcode(zipcode)
+# def legislator_by_zipcode(zipcode)
 
-  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-  civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
+#   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+#   civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
 
-  begin
-    legislators = civic_info.representative_info_by_address(
-      address: zipcode,
-      levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials
+#   begin
+#     legislators = civic_info.representative_info_by_address(
+#       address: zipcode,
+#       levels: 'country',
+#       roles: ['legislatorUpperBody', 'legislatorLowerBody']
+#     )
+#     legislators = legislators.officials
 
-    # legislator_names = legislators.map do |legislator|
-    #   legislator.name
-    # end
+#     # legislator_names = legislators.map do |legislator|
+#     #   legislator.name
+#     # end
 
-    legislator_names = legislators.map(&:name)
+#     legislator_names = legislators.map(&:name)
 
-    legislator_names.join(", ")
-  rescue 
-    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
-  end
-end
+#     legislator_names.join(", ")
+#   rescue 
+#     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+#   end
+# end
 
-contents = CSV.open(
-  "event_attendees.csv",
-  headers: true,
-  header_converters: :symbol
-)
+# contents = CSV.open(
+#   "event_attendees.csv",
+#   headers: true,
+#   header_converters: :symbol
+# )
 
-contents.each do |row|
-  name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
-  legislators = legislator_by_zipcode(zipcode)
+# contents.each do |row|
+#   name = row[:first_name]
+#   zipcode = clean_zipcode(row[:zipcode])
+#   legislators = legislator_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
-end
+#   puts "#{name} #{zipcode} #{legislators}"
+# end
