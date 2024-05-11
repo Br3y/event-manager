eafa75d1ccbb6557e8c1c6ab56c29b6a0621700f
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-05-11 11:25:12 +0800

    moviding displaying legislators to a method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -99,23 +99,15 @@ puts 'Event Manager Initialized!'
 
 # Iteration 3: Using Google's Civic Information
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
-
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
 end
 
-contents = CSV.open(
-  "event_attendees.csv",
-  headers: true,
-  header_converters: :symbol
-)
+def legislator_by_zipcode(zipcode)
 
-contents.each do |row|
-  name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
-  
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
+
   begin
     legislators = civic_info.representative_info_by_address(
       address: zipcode,
@@ -134,6 +126,18 @@ contents.each do |row|
   rescue 
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
+end
 
-  puts "#{name} #{zipcode} #{legislator_string}"
+contents = CSV.open(
+  "event_attendees.csv",
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislator_by_zipcode(zipcode)
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
