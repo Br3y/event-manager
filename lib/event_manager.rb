4664544147122ecbe687fc24d71fc034fd9f82b2
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-05-12 12:18:20 +0800

    feat: Iteration 4: Form Letters

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -141,3 +141,40 @@ puts 'Event Manager Initialized!'
 
 #   puts "#{name} #{zipcode} #{legislators}"
 # end
+
+# Iteration 4: Form Letters
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+def legislator_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
+  
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+
+    legislator_names = legislators.map(&:name).join(", ")
+  rescue  
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+contents = CSV.open(
+  "event_attendees.csv",
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislator_by_zipcode(zipcode)
+  
+  puts "#{name} #{zipcode} #{legislators}"
+end
