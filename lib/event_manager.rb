d738e6ffe54620edcf13a36f9cc0046ce5c692a4
Aubrey <lizardoaubrey.software@gmail.com>
2024-06-08 21:25:57 +0800

    feat: bundle exec rubocop -A
    three files are affected.
    lib/event_manager.rb, Gemfile and ruby_erb.rb

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,9 @@
-require 'csv'
-require 'google/apis/civicinfo_v2'
-require 'erb'
-require 'date'
+require "csv"
+require "google/apis/civicinfo_v2"
+require "erb"
+require "date"
 
-puts 'Event Manager Initialized!'
+puts "Event Manager Initialized!"
 
 # Iteration 0: Loading a file
 
@@ -29,21 +29,20 @@ puts 'Event Manager Initialized!'
 #   puts name
 # end
 
-# # Skipping the header row 
+# # Skipping the header row
 # lines = File.readlines("event_attendees.csv")
 # # row_index = 0
 # lines.each_with_index do |line, index|
 #   # next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
 #   # row_index = row_index + 1
 #   # next if row_index == 1
-#   next if index == 0 
+#   next if index == 0
 #   columns = line.split(",")
 #   name = columns[2]
 #   puts name
 # end
 
 # "Look for a solution before building a solution"
-
 
 # Iteration 1: Parsing with CSV
 
@@ -78,7 +77,6 @@ puts 'Event Manager Initialized!'
 #   puts "#{name} #{zip}"
 # end
 
-
 # Iteration 2: Cleaning up our zip codes
 
 # def clean_zipcode(zip)
@@ -97,7 +95,6 @@ puts 'Event Manager Initialized!'
 
 #   puts "#{name} #{zip}"
 # end
-
 
 # Iteration 3: Using Google's Civic Information
 
@@ -125,7 +122,7 @@ puts 'Event Manager Initialized!'
 #     legislator_names = legislators.map(&:name)
 
 #     legislator_names.join(", ")
-#   rescue 
+#   rescue
 #     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
 #   end
 # end
@@ -156,35 +153,32 @@ end
 def legislator_by_zipcode(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
-  
+
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zipcode,
-      levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+      levels: "country",
+      roles: %w[legislatorUpperBody legislatorLowerBody]
     ).officials
     # legislators = legislators.officials
 
     # legislators = legislators.map(&:name).join(", ")
-  rescue  
-    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  rescue StandardError
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
   end
 end
 
 def save_thank_you_letter(id, form_letter)
-
-  Dir.mkdir("output") unless Dir.exist?("output") 
+  FileUtils.mkdir_p("output")
 
   filename = "output/thanks_#{id}.html"
 
   File.open(filename, "w") do |file|
-    file.puts form_letter  
+    file.puts form_letter
   end
-  
 end
 
 def clean_homephone(phone)
-
   # verify_phone = phone.gsub(" ", "").gsub(".", "").gsub("-", "").gsub("(", "").gsub(")", "")
   verify_phone = phone.gsub(/[\s.\-()]/, "")
 
@@ -202,28 +196,27 @@ def clean_homephone(phone)
   #   end
   # elsif verify_phone >= 12
   #   phone += " - Bad Number"
-  # end 
+  # end
 
-  case verify_phone.length
-  when 0..9
-    phone += " - Bad Number"
-  when 10
-    phone += " - Good Nmber"
-  when 11
-    phone += verify_phone[0] == "1" ? " - Good Number" : " - Bad Number"
-  else
-    phone += " - Bad Number"
-  end
+  phone + case verify_phone.length
+          when 0..9
+            " - Bad Number"
+          when 10
+            " - Good Nmber"
+          when 11
+            verify_phone[0] == "1" ? " - Good Number" : " - Bad Number"
+          else
+            " - Bad Number"
+          end
 end
 
 def clean_registration_hours(regdate, registration_hours)
-  
   reg_hour = regdate.hour
   registration_hours[reg_hour] += 1
 
-  peak_reg_hour = registration_hours.max_by { |hour, count| count }[0]
-  peak_reg_hour_count = registration_hours.max_by { |hour, count| count }[1]
-  
+  peak_reg_hour = registration_hours.max_by { |_hour, count| count }[0]
+  peak_reg_hour_count = registration_hours.max_by { |_hour, count| count }[1]
+
   puts "The peak registration hour is: #{peak_reg_hour} with count of #{peak_reg_hour_count}"
 end
 
@@ -235,7 +228,7 @@ contents = CSV.open(
 
 registration_hours = Hash.new(0)
 registration_days = Hash.new(0)
-days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+days = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday]
 
 contents.each do |row|
   id = row[0]
@@ -245,7 +238,7 @@ contents.each do |row|
   phone = clean_homephone(row[:homephone])
   regdate = clean_registration_hours(DateTime.strptime(row[:regdate], "%m/%d/%y %H:%S"), registration_hours)
   regday = DateTime.strptime(row[:regdate], "%m/%d/%y %H:%S").wday
-  
+
   puts "#{regday} = #{days[regday]}"
 
   registration_days[regday] += 1
@@ -253,17 +246,17 @@ contents.each do |row|
   # personal_letter = template_letter.gsub("FIRST_NAME", name)
   # personal_letter.gsub!('LEGISLATORS', legislators)
   # personal_letter = personal_letter.gsub("LEGISLATORS", legislators)
-  
+
   form_letter = erb_template.result(binding)
-  
+
   save_thank_you_letter(id, form_letter)
-  
+
   puts phone
-  
+
   # puts "#{name} #{zipcode} #{legislators}"
 end
 
-peak_reg_days = registration_days.max_by { |day, count| count }[0]
+peak_reg_days = registration_days.max_by { |_day, count| count }[0]
 
 puts "The peak registration hour is: #{days[peak_reg_days]}"
 
